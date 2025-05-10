83a509445441be0da75a7fc5fb9ef52da4c6f25f
root-zero-o <rootzero17@gmail.com>
2025-05-10 22:15:14 +0900

    사각형 그리기 성공

--- webgpu_prac1.js
+++ webgpu_prac1.js
@@ -40,6 +40,87 @@ const pass = encoder.beginRenderPass({
   ],
 });
 
+// -------------
+// 도형 그리기
+// -------------
+
+// 1. 사각형의 꼭짓점 정의
+
+// const vertices = new Float32Array([-0.8, -0.8, 0.8, -0.8, 0.8, 0.8, -0.8, 0.8]);
+// -> GPU는 삼각형을 기준으로 작동하므로 삼각형 2개가 필요함
+
+// Triangle 1
+const vertices = new Float32Array([
+  -0.8, -0.8, 0.8, -0.8, 0.8, 0.8,
+  // Triangle 2
+  -0.8, -0.8, 0.8, 0.8, -0.8, 0.8,
+]);
+
+// 2. 꼭짓점 버퍼 만들기
+const vertexBuffer = device.createBuffer({
+  lable: "Cell vertices",
+  size: vertices.byteLength,
+  usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+});
+
+// 3. 꼭짓점 데이터를 버퍼의 메모리에 복사
+device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/ 0, vertices);
+
+// 4. 꼭짓점 레이아웃 정의
+const vertexBufferLayout = {
+  arrayStride: 8, // GPU가 다음 꼭짓점을 찾을 때 버퍼에서 앞으로 건너뛰어야 하는 바이트 수
+  attributes: [
+    // 각 꼭짓점으로 인코딩된 개별 정보
+    {
+      format: "float32x2", // 꼭짓점 데이터 유형
+      offset: 0, // 특정 속성이 시작하는 꼭짓점에 포함된 바이트 수를 설명
+      shaderLocation: 0, // Position, see vertex shader
+    },
+  ],
+};
+
+// 5. shader
+// WGSL이라는 셰이딩 언어로 작성됨
+const cellShaderModule = device.createShaderModule({
+  label: "Cell shader",
+  code: `
+    @vertex
+    fn vertexMain(@location(0) pos: vec2f) ->
+      @builtin(position) vec4f {
+      return vec4f(pos.x, pos.y, 0, 1); 
+    }
+
+    @fragment
+    fn fragmentMain() -> @location(0) vec4f {
+      return vec4f(1, 0, 0, 1);
+    }
+  `,
+});
+
+// 6. 렌더링 파이프라인
+const cellPipeline = device.createRenderPipeline({
+  label: "Cell pipeline",
+  layout: "auto",
+  vertex: {
+    module: cellShaderModule,
+    entryPoint: "vertexMain",
+    buffers: [vertexBufferLayout],
+  },
+  fragment: {
+    module: cellShaderModule,
+    entryPoint: "fragmentMain",
+    targets: [
+      {
+        format: canvasFormat,
+      },
+    ],
+  },
+});
+
+pass.setPipeline(cellPipeline);
+pass.setVertexBuffer(0, vertexBuffer);
+pass.draw(vertices.length / 2); // 6 vertices
+
 // 5-3. 렌더 패스 종료
 pass.end();
 

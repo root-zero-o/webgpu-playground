e642aca035cfa4eec1da34d5d25095805e4642b5
root-zero-o <rootzero17@gmail.com>
2025-05-10 21:26:47 +0900

    사각형을 그렸다 !

--- webgpu_prac1.js
+++ webgpu_prac1.js
@@ -24,4 +24,24 @@ context.configure({
   format: canvasFormat,
 });
 
-console.log(1);
+// 5. GPU 명령어를 기록하기 (한번에 모아서 보냄)
+// 5-1. 명령 기록 시작
+const encoder = device.createCommandEncoder();
+
+// 5-2. 렌더 패스 설정
+const pass = encoder.beginRenderPass({
+  colorAttachments: [
+    {
+      view: context.getCurrentTexture().createView(),
+      loadOp: "clear", // 렌더 패스 시작 시 텍스처를 지움
+      clearValue: [0, 0.5, 0.7, 1],
+      storeOp: "store", // 렌더 패스 완료 시 렌더 패스 중 그리는 결과가 텍스처에 저장됨
+    },
+  ],
+});
+
+// 5-3. 렌더 패스 종료
+pass.end();
+
+// 5-4. Finish the command buffer and immediately submit it.
+device.queue.submit([encoder.finish()]);
